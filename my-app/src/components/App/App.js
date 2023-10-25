fc7242ac00a5bd13cec116c949efc4ab96fae922
Amr Hweedy <amr.hweedy@iken.tech>
2023-10-25 06:52:47 -0700

    third

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -6,15 +6,41 @@ import Card from '../Card/Card';
 // the second parameter(setstate) is used to update the data for example if i need to add value inside the array i use setstate instead of push
 const App = () => {
 
-  const[state,setState]=useState(   // i make destruction for usestate()
-    [{name:"amr",age:26},{name:"ahmed",age:25},{name:"ali",age:30}]  
- )
+const[cardToggle,setCardToggle]=useState(false);
 
- 
-  return (
+  const[state,setState]=useState(   // i make destruction for usestate()
+    [{name:"amr",age:26},{name:"ahmed",age:25},{name:"ali",age:30}])
+
+ const deleteHandler = (event,clickIndex)=>{
+
+  //const reminder = state.filter((ele,index)=> clickIndex !==index )
+  //setState(reminder);  // ميتفعش اغير في الستيت علطول المفروض اخد منه نسخه الاول وبعدين اعدل عليها وبعدين ارجع النسخه بعد التعديل ذي ما عملت هنا 
+ // console.log(state)  // لو عندي 3 عناصر ومسحت عنصر وطبعت الستيت هتجبلي الاراي وفيها ال 3 عناصر يعني بتجبلي القيمه اللي اتمسحت كمان لان الستيبت هي اسينك يعني مش ضامن انها ترجعلي اخر قيمه بس ودا اللي بيحصل هنا بترجعلي القيمه اللي اتمسحت كمان
+
+// another solution
+ setState((prevState)=>{
+  return prevState.filter((ele,index)=> clickIndex!==index);
+ })
+ console.log("inside",state) // هيطبع الستيت القديم يعني ال 3 عناصر 
+}
+
+console.log("outside",state) //هيطبع احدث ستيت يعني هيطبع عنصرين بس
+
+const handlerToggle =()=>{
+  setCardToggle(!cardToggle);}
+
+  return (      // here there is the latest updated version of state 
+               //اللي مكتوب داخل الرترن هو اللي هيتعرض
     <div className='mainContent'>
     <h1>boys</h1>
-    <Card namesList={state} type="men"></Card> 
+    {
+      // another solution for onclick
+      //<button style={{marginBottom:"20px"}} onClick={handlerToggle}>{cardToggle?"hide names" :"show names"} </button>
+    } 
+    <button style={{marginBottom:"20px"}} onClick={()=>setCardToggle(!cardToggle)}>{cardToggle?"hide names" :"show names"} </button>
+    <div className={cardToggle? "show" :"hide"}>
+       <Card namesList={state} type="men" deleteHandler={deleteHandler} ></Card> 
+    </div>
     </div>
   )
 }
--- my-app/src/components/Card/Card.js
+++ my-app/src/components/Card/Card.js
@@ -2,8 +2,7 @@ import React from "react"
 import styles from "./Card.module.css"
 
 const Card = (props)=>{      // here the props is object contains the array (datalist) which is in the app component (parent) and i send it to the card comp (child)
-    console.log (props)
-    
+      
     const cards =  props.namesList.map((ele , index)=>( 
     <div 
       className={styles.cardWrapper}
@@ -11,7 +10,7 @@ const Card = (props)=>{      // here the props is object contains the array (dat
       style={{background: props.type==="men"? "green" :"pink"}}>
        <div>{ele.name}</div>
        <div>{ele.age}</div>
-       <div className={styles.deleteButton}>x</div>
+       <div className={styles.deleteButton} onClick={(event)=>props.deleteHandler(event , index)}>x</div>
 </div> ));
 
     return <div>{cards}</div>   
